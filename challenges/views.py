a8d92df91474c2c65ebd05b2781c035af0323c82
C.Reyes <cvreyes@hotmail.com>
2026-04-01 16:53:30 +0800

    No. 23

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
+monthly_challenges = {
+    "january": "January Challenges!",
+    "february": "February Challenges!",
+    "march": "March Challenges!",
+    "april": "April Challenges!",
+    "may": "May Challenges!",
+    "june": "June Challenges!",
+    "july": "July Challenges!",
+    "august": "August Challenges!",
+    "september": "September Challenges!",
+    "october": "October Challenges!",
+    "november": "November Challenges!",
+    "december": "December Challenges!",
+}
 
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("January")
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
